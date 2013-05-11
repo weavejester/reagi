10986f71077725d14d54967b7225c6050bb7017d
James Reeves <jreeves@weavejester.com>
2013-05-11 21:10:04 +0100

    Initial commit

---
+++ src/reagi/core.clj
@@ -0,0 +1,6 @@
+(ns reagi.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/reagi/core_test.clj
@@ -0,0 +1,7 @@
+(ns reagi.core-test
+  (:require [clojure.test :refer :all]
+            [reagi.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
