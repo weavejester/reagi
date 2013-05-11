eef5cd12408ed1f97739a90ab3a258c573fec305
James Reeves <jreeves@weavejester.com>
2013-05-11 22:52:35 +0100

    Added delta function for describing change over time

--- src/reagi/core.clj
+++ src/reagi/core.clj
@@ -11,3 +11,13 @@
 
 (defmacro behavior [& form]
   `(behavior-call (fn [] ~@form)))
+
+(defn delta [f init]
+  (let [last-time-ms (atom (System/currentTimeMillis))
+        last-result  (atom init)]
+    (behavior
+     (let [time-ms   (System/currentTimeMillis)
+           change-ms (- time-ms @last-time-ms)
+           result    (f @last-result (/ change-ms 1000.0))]
+       (reset! last-time-ms time-ms)
+       (reset! last-result result)))))
