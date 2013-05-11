1b0258c6a254bd51141749597afa550aaa14ab92
James Reeves <jreeves@weavejester.com>
2013-05-11 22:38:18 +0100

    Ensure derefing is consistent within a behavior

--- src/reagi/core.clj
+++ src/reagi/core.clj
@@ -1,9 +1,13 @@
 (ns reagi.core)
 
+(def ^:dynamic *behaviors* nil)
+
 (defn behavior-call [f]
   (reify
     clojure.lang.IDeref
-    (deref [_] (f))))
+    (deref [_]
+      (binding [*behaviors* (or *behaviors* (memoize #(%)))]
+        (*behaviors* f)))))
 
 (defmacro behavior [& form]
   `(behavior-call (fn [] ~@form)))
