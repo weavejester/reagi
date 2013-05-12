9ea701ddaf8237e86b5beb48984b57ca5ee2419d
James Reeves <jreeves@weavejester.com>
2013-05-12 02:04:41 +0100

    Added docstrings

--- src/reagi/core.clj
+++ src/reagi/core.clj
@@ -2,17 +2,29 @@
 
 (def ^:dynamic *behaviors* nil)
 
-(defn behavior-call [f]
+(defn behavior-call
+  "Takes a zero-argument function and yields a behavior object that will
+  evaluate the function each time it is dereferenced. See: behavior."
+  [f]
   (reify
     clojure.lang.IDeref
     (deref [_]
       (binding [*behaviors* (or *behaviors* (memoize #(%)))]
         (*behaviors* f)))))
 
-(defmacro behavior [& form]
+(defmacro behavior
+  "Takes a body of expressions and yields a behavior object that will evaluate
+  the body each time it is dereferenced. All derefs of behaviors that happen
+  inside a containing behavior will be consistent."
+  [& form]
   `(behavior-call (fn [] ~@form)))
 
-(defn delta [f init]
+(defn delta
+  "Constructs a behavior from an initial value and a function that describes an
+  accumulative change to that value over time. The function should take two
+  arguments, the current value and the number of seconds since it was last
+  called, and return a new value."
+  [f init]
   (let [last-time-ms (atom (System/currentTimeMillis))
         last-result  (atom init)]
     (behavior
