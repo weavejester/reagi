f3be3f1da5b9cd3b417296e89a21800d0efdde8d
James Reeves <jreeves@weavejester.com>
2013-05-12 17:12:52 +0100

    Removed delta function from core

--- src/reagi/core.clj
+++ src/reagi/core.clj
@@ -19,17 +19,3 @@
   [& form]
   `(behavior-call (fn [] ~@form)))
 
-(defn delta
-  "Constructs a behavior from an initial value and a function that describes an
-  accumulative change to that value over time. The function should take two
-  arguments, the current value and the number of seconds since it was last
-  called, and return a new value."
-  [f init]
-  (let [last-time-ms (atom (System/currentTimeMillis))
-        last-result  (atom init)]
-    (behavior
-     (let [time-ms   (System/currentTimeMillis)
-           change-ms (- time-ms @last-time-ms)
-           result    (f @last-result (/ change-ms 1000.0))]
-       (reset! last-time-ms time-ms)
-       (reset! last-result result)))))
