8ff0becf180d7a64683812c51ebc1414b5a5cf21
James Reeves <jreeves@weavejester.com>
2013-05-12 21:49:58 +0100

    Updated project.clj with description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reagi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Very experimental FRP library"
+  :url "https://github.com/weavejester/reagi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]])
