c5a84cbcaf697c7368c4d9ba57d83529c110c719
James Reeves <jreeves@weavejester.com>
2013-05-12 19:25:27 +0100

    Added Stream protocol, map, filter and mapcat

--- src/reagi/core.clj
+++ src/reagi/core.clj
@@ -1,5 +1,5 @@
 (ns reagi.core
-  (:refer-clojure :exclude [reduce map filter merge]))
+  (:refer-clojure :exclude [mapcat map filter]))
 
 (def ^:dynamic *behaviors* nil)
 
@@ -20,23 +20,45 @@
   [& form]
   `(behavior-call (fn [] ~@form)))
 
+(defprotocol Stream
+  (push! [stream msg] "Push a message onto the stream.")
+  (subscribe [stream listener] "Add a listener function to the stream."))
+
 (defrecord EventStream [observers head]
   clojure.lang.IDeref
-  (deref [_] @head))
+  (deref [_] @head)
+  Stream
+  (push! [stream msg]
+    (reset! head msg)
+    (doseq [[observer _] observers]
+      (observer msg)))
+  (subscribe [stream observer]
+    (.put observers observer true)))
 
-(defn event-stream [init]
-  (EventStream. (java.util.WeakHashMap.) (atom init)))
+(defn push-seq!
+  "Push a seq of messages to an event stream."
+  [stream msgs]
+  (doseq [m msgs]
+    (push! stream m)))
 
-(defn push! [stream value]
-  (reset! (:head stream) value)
-  (doseq [[observer _] (:observers stream)]
-    (observer value)))
+(defn event-stream
+  "Create a new event stream with an optional initial value."
+  ([] (event-stream nil))
+  ([init] (EventStream. (java.util.WeakHashMap.) (atom init))))
 
-(defn- subscribe [stream f]
-  (.put (:observers stream) f true))
+(defn mapcat
+  "Mapcat a function over a stream."
+  [f stream]
+  (let [stream* (event-stream)]
+    (subscribe stream #(push-seq! stream* (f %)))
+    stream*))
 
-(defn reduce
-  [f init stream]
-  (let [reduced (event-stream init)]
-    (subscribe stream #(push! reduced (f @(:head reduced) %)))
-    reduced))
+(defn map
+  "Map a function over a stream."
+  [f stream]
+  (mapcat #(list (f %)) stream))
+
+(defn filter
+  "Filter a stream by a predicate."
+  [pred stream]
+  (mapcat #(if (pred %) (list %)) stream))
