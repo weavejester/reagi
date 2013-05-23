8c1c25108b2ee764930c44a5a8a7bccfb59ca13b
James Reeves <jreeves@weavejester.com>
2013-05-23 22:44:39 +0100

    Released 0.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagi "0.0.5"
+(defproject reagi "0.0.6"
   :description "Very experimental FRP library"
   :url "https://github.com/weavejester/reagi"
   :license {:name "Eclipse Public License"
