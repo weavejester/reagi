56d339dd4bf04168d009878d1c567bac6d0b8c55
James Reeves <jreeves@weavejester.com>
2013-05-24 00:48:03 +0100

    Added some tests for event-stream

--- test/reagi/core_test.clj
+++ test/reagi/core_test.clj
@@ -8,3 +8,14 @@
     (is (= @b 2))
     (swap! a inc)
     (is (= @b 3))))
+
+(deftest test-event-stream
+  (testing "Initial value"
+    (is (nil? @(r/event-stream)))
+    (is (= 1 @(r/event-stream 1))))
+  (testing "Push"
+    (let [e (r/event-stream)]
+      (r/push! e 1)
+      (is (= 1 @e))
+      (r/push! e 2)
+      (is (= 2 @e)))))
