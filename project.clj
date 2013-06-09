117fa39bb08b5eb805ed54a9c2bd385ad80dc462
James Reeves <jreeves@weavejester.com>
2013-06-09 17:20:41 +0100

    Added travis.yml file

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :plugins [[codox "0.6.4"]])
+  :plugins [[codox "0.6.4"]]
+  :profiles
+  {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
