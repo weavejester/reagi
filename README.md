2466b5f84053f9055bbbb6de43919078a7f3867a
James Reeves <jreeves@weavejester.com>
2013-06-28 02:16:06 +0100

    Released 0.4.0

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ stone. Things may change.
 
 Add the following dependency to your `project.clj` file:
 
-    [reagi "0.3.1"]
+    [reagi "0.4.0"]
 
 ## Documentation
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagi "0.3.1"
+(defproject reagi "0.4.0"
   :description "An experimental FRP library"
   :url "https://github.com/weavejester/reagi"
   :license {:name "Eclipse Public License"
