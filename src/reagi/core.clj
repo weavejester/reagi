29959df6c0b290abcfec0523c4001f7edea35599
mopemoepe <yutaka.matsubara@gmail.com>
2013-07-12 12:21:36 +0900

    Fix reflection

--- src/reagi/core.clj
+++ src/reagi/core.clj
@@ -50,7 +50,7 @@
 (defprotocol Observable
   (subscribe [stream observer] "Add an observer function to the stream."))
 
-(defn- weak-hash-map []
+(defn- ^java.util.Map weak-hash-map []
   (java.util.Collections/synchronizedMap (java.util.WeakHashMap.)))
 
 (defn event-stream
