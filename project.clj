a8bdcda68f3aa8c6ceba6229c9efea6e66839287
James Reeves <jreeves@weavejester.com>
2013-09-18 06:20:22 +0100

    Added core.async dependency

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "https://github.com/weavejester/reagi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]]
   :plugins [[codox "0.6.4"]]
   :profiles
-  {:dev {:dependencies [[criterium "0.4.1"]]}
-   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+  {:dev {:dependencies [[criterium "0.4.1"]]}})
