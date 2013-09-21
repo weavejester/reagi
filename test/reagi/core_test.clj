11b6cec4e76c09677d3d9a2ca97f6b019b97394b
James Reeves <jreeves@weavejester.com>
2013-09-21 07:54:07 +0100

    Fixed a number of tests broken by move to async
    A small delay after pushing an event to a stream ensures the value is
    correct when we dereference.

--- test/reagi/core_test.clj
+++ test/reagi/core_test.clj
@@ -16,15 +16,21 @@
   (testing "Push"
     (let [e (r/event-stream)]
       (e 1)
+      (Thread/sleep 20)
       (is (= 1 @e))
       (e 2)
+      (Thread/sleep 20)
       (is (= 2 @e)))))
+
+(defn- push!! [stream & msgs]
+  (apply r/push! stream msgs)
+  (Thread/sleep 20))
 
 (deftest test-push!
   (let [e (r/event-stream)]
-    (r/push! e 1)
+    (push!! e 1)
     (is (= 1 @e))
-    (r/push! e 2 3 4)
+    (push!! e 2 3 4)
     (is (= 4 @e))))
 
 (deftest test-initial
@@ -32,7 +38,7 @@
         e2 (r/initial :bar e1)]
     (is (= @e1 :foo))
     (is (= @e2 :bar))
-    (r/push! e1 :baz)
+    (push!! e1 :baz)
     (is (= @e1 :baz))
     (is (= @e2 :baz))))
 
@@ -41,12 +47,12 @@
         e2 (r/event-stream 0)
         z  (r/zip e1 e2)]
     (is (= @z [0 0]))
-    (r/push! e1 1)
+    (push!! e1 1)
     (is (= @z [1 0]))
-    (r/push! e2 2)
+    (push!! e2 2)
     (is (= @z [1 2]))
-    (r/push! e1 3)
-    (r/push! e2 4)
+    (push!! e1 3)
+    (push!! e2 4)
     (is (= @z [3 4]))))
 
 (deftest test-map
@@ -54,16 +60,16 @@
     (let [s (r/event-stream 0)
           e (r/map inc s)]
       (is (= 1 @e))
-      (r/push! s 1)
+      (push!! s 1)
       (is (= 2 @e))))
   (testing "Multiple streams"
     (let [s1 (r/event-stream 0)
           s2 (r/event-stream 0)
           e  (r/map + s1 s2)]
       (is (= @e 0))
-      (r/push! s1 4)
+      (push!! s1 4)
       (is (= @e 4))
-      (r/push! s2 6)
+      (push!! s2 6)
       (is (= @e 10)))))
 
 (deftest test-mapcat
@@ -71,47 +77,47 @@
     (let [s (r/event-stream 0)
           e (r/mapcat (comp list inc) s)]
       (is (= 1 @e))
-      (r/push! s 1)
+      (push!! s 1)
       (is (= 2 @e))))
   (testing "No initial value"
     (let [s (r/event-stream)
           e (r/mapcat (comp list inc) s)]
-      (r/push! s 1)
+      (push!! s 1)
       (is (= 2 @e))))
   (testing "Multiple streams"
     (let [s1 (r/event-stream 0)
           s2 (r/event-stream 0)
           e  (r/mapcat (comp list +) s1 s2)]
       (is (= @e 0))
-      (r/push! s1 2)
+      (push!! s1 2)
       (is (= @e 2))
-      (r/push! s2 3)
+      (push!! s2 3)
       (is (= @e 5)))))
 
 (deftest test-uniq
   (let [s (r/event-stream)
         e (r/reduce + 0 (r/uniq s))]
-    (r/push! s 1 1)
+    (push!! s 1 1)
     (is (= 1 @e))
-    (r/push! s 1 2)
+    (push!! s 1 2)
     (is (= 3 @e))))
 
 (deftest test-count
   (let [e (r/event-stream)
         c (r/count e)]
     (is (= @c 0))
-    (r/push! e 1)
+    (push!! e 1)
     (is (= @c 1))
-    (r/push! e 2 3)
+    (push!! e 2 3)
     (is (= @c 3))))
 
 (deftest test-cycle
   (let [s (r/event-stream)
         e (r/cycle [:on :off] s)]
     (is (= :on @e))
-    (r/push! s 1)
+    (push!! s 1)
     (is (= :off @e))
-    (r/push! s 1)
+    (push!! s 1)
     (is (= :on @e))))
 
 (deftest test-constantly
@@ -119,7 +125,7 @@
         e (r/constantly 1 s)
         a (r/reduce + 0 e)]
     (is (= @e 1))
-    (r/push! s 2 4 5)
+    (push!! s 2 4 5)
     (is (= @e 1))
     (is (= @a 3))))
 
@@ -139,26 +145,26 @@
     (let [s (r/event-stream 0)
           e (r/map inc (r/map inc s))]
       (System/gc)
-      (r/push! s 1)
+      (push!! s 1)
       (is (= @e 3))))
   (testing "Merge"
     (let [s (r/event-stream 0)
           e (r/merge (r/map inc s))]
       (System/gc)
-      (r/push! s 1)
+      (push!! s 1)
       (is (= @e 2))))
   (testing "Zip"
     (let [s (r/event-stream 0)
           e (r/zip (r/map inc s) (r/map dec s))]
       (System/gc)
-      (r/push! s 1)
+      (push!! s 1)
       (is (= @e [2 0]))))
   (testing "GC unreferenced streams"
     (let [a (atom nil)
           s (r/event-stream)]
       (r/map #(reset! a %) s)
       (System/gc)
-      (r/push! s 1)
+      (push!! s 1)
       (is (nil? @a)))))
 
 (deftest test-sample
