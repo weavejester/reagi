203131b1e2962abe3aa0f71ee76259b483e7bbdd
James Reeves <jreeves@weavejester.com>
2014-02-02 20:01:42 +0000

    Updated dependencies

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
-                 [org.clojure/clojurescript "0.0-2120"]]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/clojurescript "0.0-2156"]]
   :plugins [[codox "0.6.6"]
             [com.cemerick/austin "0.1.3"]
-            [lein-cljsbuild "1.0.0"]]
+            [lein-cljsbuild "1.0.1"]]
   :source-paths ["src/clojure" "src/cljs"]
   :cljsbuild
   {:builds [{:source-paths ["src/clojure" "src/cljs"]
