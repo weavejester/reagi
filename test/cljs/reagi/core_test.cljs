43203f64a7ad2705c12251dacbb4b1b8505797c8
James Reeves <jreeves@weavejester.com>
2014-02-14 19:36:08 +0000

    Added cljs behavior tests

--- test/cljs/reagi/core_test.cljs
+++ test/cljs/reagi/core_test.cljs
@@ -1,6 +1,15 @@
 (ns reagi.core-test
   (:require-macros [cemerick.cljs.test :refer (is deftest testing)])
-  (:require [cemerick.cljs.test :as t]))
+  (:require [cemerick.cljs.test :as t]
+            [reagi.core :as r :include-macros true]))
 
-(deftest foo-test
-  (is (= 1 1)))
+(deftest test-behavior
+  (let [a (atom 1)
+        b (r/behavior (+ 1 @a))]
+    (is (= @b 2))
+    (swap! a inc)
+    (is (= @b 3))))
+
+(deftest test-behavior
+  (is (r/behavior? (r/behavior "foo")))
+  (is (not (r/behavior? "foo"))))
