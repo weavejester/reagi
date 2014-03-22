012b31289c034951da5fa4123ba287019c8c095c
James Reeves <jreeves@weavejester.com>
2014-03-22 16:54:49 +0000

    Updated clojurescript and core.async dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [org.clojure/clojurescript "0.0-2156"]]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+                 [org.clojure/clojurescript "0.0-2173"]]
   :plugins [[codox "0.6.6"]
             [lein-cljsbuild "1.0.2"]]
   :source-paths ["src/clojure" "src/cljs"]
