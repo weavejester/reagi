713b544c3328319d6d0853639fc38e131953a36f
James Reeves <jreeves@weavejester.com>
2014-04-26 02:34:16 +0100

    Updated core.async version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+                 [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]
                  [org.clojure/clojurescript "0.0-2202"]]
   :plugins [[codox "0.6.7"]
             [lein-cljsbuild "1.0.3"]]
