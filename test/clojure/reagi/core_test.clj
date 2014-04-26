e3f37af468f2aa70624138379a8ec86d340be593
James Reeves <jreeves@weavejester.com>
2014-04-26 02:46:43 +0100

    Added timeouts to all derefs in Clojure tests
    If there's a bug we don't want the tests to hang.

--- test/clojure/reagi/core_test.clj
+++ test/clojure/reagi/core_test.clj
@@ -33,15 +33,18 @@
   (is (= (r/unbox (r/box nil)) nil))
   (is (= (r/unbox (r/box 1)) 1)))
 
+(defn- deref! [e]
+  (deref e 10000 :timeout))
+
 (deftest test-events
   (testing "push"
     (let [e (r/events)]
       (e 1)
       (Thread/sleep 20)
-      (is (= 1 @e))
+      (is (= (deref! e) 1))
       (e 2)
       (Thread/sleep 20)
-      (is (= 2 @e))))
+      (is (= (deref! e) 2))))
   (testing "realized?"
     (let [e (r/events)]
       (is (not (realized? e)))
@@ -63,7 +66,7 @@
       (>!! ch :foo)
       (Thread/sleep 20)
       (is (realized? e))
-      (is (= @e :foo)))))
+      (is (= (deref! e) :foo)))))
 
 (deftest test-events?
   (is (r/events? (r/events)))
@@ -76,9 +79,9 @@
 (deftest test-push!
   (let [e (r/events)]
     (push!! e 1)
-    (is (= 1 @e))
+    (is (= (deref! e) 1))
     (push!! e 2 3 4)
-    (is (= 4 @e))))
+    (is (= (deref! e) 4))))
 
 (deftest test-completed
   (testing "behaviors"
@@ -97,30 +100,30 @@
   (testing "events"
     (let [e (r/events)]
       (push!! e 1)
-      (is (= @e 1))
+      (is (= (deref! e) 1))
       (push!! e (r/completed 2))
-      (is (= @e 2))
+      (is (= (deref! e) 2))
       (is (r/complete? e))
       (push!! e 3)
-      (is (= @e 2))))
+      (is (= (deref! e) 2))))
   (testing "derived events"
     (let [e (r/events)
           m (r/map inc e)]
       (push!! e 1)
-      (is (= @m 2))
+      (is (= (deref! m) 2))
       (push!! e (r/completed 2))
-      (is (= @m 3))
+      (is (= (deref! m) 3))
       (is (r/complete? m))
       (push!! e 3)
-      (is (= @m 3))))
+      (is (= (deref! m) 3))))
   (testing "channel events"
     (let [c (chan)
           e (r/events c)]
       (>!! c 1)
-      (is (= @e 1))
+      (is (= (deref! e) 1))
       (is (not (r/complete? e)))
       (close! c)
-      (is (= @e 1))
+      (is (= (deref! e) 1))
       (is (r/complete? e)))))
 
 (deftest test-sink!
@@ -142,9 +145,9 @@
   (let [e (r/events)
         c (r/cons 5 e)]
     (is (realized? c))
-    (is (= @c 5))
+    (is (= (deref! c) 5))
     (push!! e 10)
-    (is (= @c 10))))
+    (is (= (deref! c) 10))))
 
 (deftest test-merge
   (testing "merged streams"
@@ -152,9 +155,9 @@
           e2 (r/events)
           m  (r/merge e1 e2)]
       (push!! e1 1)
-      (is (= @m 1))
+      (is (= (deref! m) 1))
       (push!! e2 2)
-      (is (= @m 2))))
+      (is (= (deref! m) 2))))
   (testing "closed channels"
     (let [ch1 (chan)
           ch2 (chan)
@@ -162,11 +165,11 @@
           e2  (r/events ch2)
           m   (r/merge e1 e2)]
       (>!! ch1 1)
-      (is (= @m 1))
+      (is (= (deref! m) 1))
       (close! ch1)
       (>!! ch2 2)
       (Thread/sleep 20)
-      (is (= @m 2)))))
+      (is (= (deref! m) 2)))))
 
 (deftest test-zip
   (testing "zipped streams"
@@ -175,11 +178,11 @@
           z  (r/zip e1 e2)]
       (push!! e1 1)
       (push!! e2 2)
-      (is (= @z [1 2]))
+      (is (= (deref! z) [1 2]))
       (push!! e1 3)
-      (is (= @z [3 2]))
+      (is (= (deref! z) [3 2]))
       (push!! e2 4)
-      (is (= @z [3 4]))))
+      (is (= (deref! z) [3 4]))))
   (testing "closed channels"
     (let [ch1 (chan)
           ch2 (chan)
@@ -188,39 +191,39 @@
           z   (r/zip e1 e2)]
       (>!! ch1 1)
       (>!! ch2 2)
-      (is (= @z [1 2]))
+      (is (= (deref! z) [1 2]))
       (close! ch1)
       (>!! ch2 3)
       (Thread/sleep 20)
-      (is (= @z [1 3])))))
+      (is (= (deref! z) [1 3])))))
 
 (deftest test-map
   (testing "Basic operation"
     (let [s (r/events)
           e (r/map inc s)]
       (push!! s 1)
-      (is (= 2 @e))))
+      (is (= (deref! e) 2))))
   (testing "Multiple streams"
     (let [s1 (r/events)
           s2 (r/events)
           e  (r/map + s1 s2)]
       (push!! s1 4)
       (push!! s2 6)
-      (is (= @e 10)))))
+      (is (= (deref! e) 10)))))
 
 (deftest test-mapcat
   (testing "Basic operation"
     (let [s (r/events)
           e (r/mapcat (comp list inc) s)]
       (push!! s 1)
-      (is (= 2 @e))))
+      (is (= (deref! e) 2))))
   (testing "Multiple streams"
     (let [s1 (r/events)
           s2 (r/events)
           e  (r/mapcat (comp list +) s1 s2)]
       (push!! s1 2)
       (push!! s2 3)
-      (is (= @e 5)))))
+      (is (= (deref! e) 5)))))
 
 (deftest test-filter
   (let [s (r/events)
@@ -228,7 +231,7 @@
     (push!! s 1)
     (is (not (realized? e)))
     (push!! s 2 3)
-    (is (= @e 2))))
+    (is (= (deref! e) 2))))
 
 (deftest test-remove
   (let [s (r/events)
@@ -236,7 +239,7 @@
     (push!! s 0)
     (is (not (realized? e)))
     (push!! s 1 2)
-    (is (= @e 1))))
+    (is (= (deref! e) 1))))
 
 (deftest test-reduce
   (testing "no initial value"
@@ -245,47 +248,47 @@
       (is (not (realized? e)))
       (push!! s 1)
       (is (realized? e))
-      (is (= @e 1))
+      (is (= (deref! e) 1))
       (push!! s 2)
-      (is (= @e 3))
+      (is (= (deref! e) 3))
       (push!! s 3 4)
-      (is (= @e 10))))
+      (is (= (deref! e) 10))))
   (testing "initial value"
     (let [s (r/events)
           e (r/reduce + 0 s)]
       (is (realized? e))
-      (is (= @e 0))
+      (is (= (deref! e) 0))
       (push!! s 1)
-      (is (= @e 1))
+      (is (= (deref! e) 1))
       (push!! s 2 3)
-      (is (= @e 6))))
+      (is (= (deref! e) 6))))
   (testing "initial value persists"
     (let [s (r/events)
           e (r/map inc (r/reduce + 0 s))]
-      (is (= (deref e 1000 :error) 1)))))
+      (is (= (deref e 1000 :timeout) 1)))))
 
 (deftest test-buffer
   (testing "unlimited buffer"
     (let [s (r/events)
           b (r/buffer s)]
-      (is (empty? @b))
+      (is (empty? (deref! b)))
       (push!! s 1)
-      (is (= @b [1]))
+      (is (= (deref! b) [1]))
       (push!! s 2 3 4 5)
-      (is (= @b [1 2 3 4 5]))))
+      (is (= (deref! b) [1 2 3 4 5]))))
   (testing "limited buffer"
     (let [s (r/events)
           b (r/buffer 3 s)]
-      (is (empty? @b))
+      (is (empty? (deref! b)))
       (push!! s 1)
-      (is (= @b [1]))
+      (is (= (deref! b) [1]))
       (push!! s 2 3 4 5)
-      (is (= @b [3 4 5]))))
+      (is (= (deref! b) [3 4 5]))))
   (testing "smallest buffer"
     (let [s (r/events)
           b (r/buffer 1 s)]
       (push!! s 2 3 4 5)
-      (is (= @b [5]))))
+      (is (= (deref! b) [5]))))
   (testing "preconditions"
     (is (thrown? AssertionError (r/buffer 0 (r/events))))
     (is (thrown? AssertionError (r/buffer 1.0 (r/events))))))
@@ -294,47 +297,47 @@
   (let [s (r/events)
         e (r/reduce + 0 (r/uniq s))]
     (push!! s 1 1)
-    (is (= 1 @e))
+    (is (= 1 (deref! e)))
     (push!! s 1 2)
-    (is (= 3 @e))))
+    (is (= 3 (deref! e)))))
 
 (deftest test-count
   (let [e (r/events)
         c (r/count e)]
-    (is (= @c 0))
+    (is (= (deref! c) 0))
     (push!! e 1)
-    (is (= @c 1))
+    (is (= (deref! c) 1))
     (push!! e 2 3)
-    (is (= @c 3))))
+    (is (= (deref! c) 3))))
 
 (deftest test-cycle
   (let [s (r/events)
         e (r/cycle [:on :off] s)]
-    (is (= :on (deref e 1000 :error)))
+    (is (= :on (deref! e)))
     (push!! s 1)
-    (is (= :off @e))
+    (is (= :off (deref! e)))
     (push!! s 1)
-    (is (= :on @e))))
+    (is (= :on (deref! e)))))
 
 (deftest test-constantly
   (let [s (r/events)
         e (r/constantly 1 s)
         a (r/reduce + 0 e)]
     (push!! s 2 4 5)
-    (is (= @e 1))
-    (is (= @a 3))))
+    (is (= (deref! e) 1))
+    (is (= (deref! a) 3))))
 
 (deftest test-throttle
   (let [s (r/events)
         e (r/throttle 100 s)]
     (r/push! s 1 2)
     (Thread/sleep 20)
-    (is (= @e 1))
+    (is (= (deref! e) 1))
     (Thread/sleep 101)
     (r/push! s 3)
     (Thread/sleep 50)
     (r/push! s 4)
-    (is (= @e 3))))
+    (is (= (deref! e) 3))))
 
 (deftest test-gc
   (testing "Derived maps"
@@ -342,19 +345,19 @@
           e (r/map inc (r/map inc s))]
       (System/gc)
       (push!! s 1)
-      (is (= @e 3))))
+      (is (= (deref! e) 3))))
   (testing "Merge"
     (let [s (r/events)
           e (r/merge (r/map inc s))]
       (System/gc)
       (push!! s 1)
-      (is (= @e 2))))
+      (is (= (deref! e) 2))))
   (testing "Zip"
     (let [s (r/events)
           e (r/zip (r/map inc s) (r/map dec s))]
       (System/gc)
       (push!! s 1)
-      (is (= @e [2 0]))))
+      (is (= (deref! e) [2 0]))))
   (testing "GC unreferenced streams"
     (let [a (atom nil)
           s (r/events)]
@@ -368,11 +371,11 @@
   (testing "Basic sample"
     (let [a (atom 0)
           s (r/sample 100 a)]
-      (is (= @s 0))
+      (is (= (deref! s) 0))
       (swap! a inc)
-      (is (= @s 0))
+      (is (= (deref! s) 0))
       (Thread/sleep 120)
-      (is (= @s 1))))
+      (is (= (deref! s) 1))))
   (testing "Thread ends if stream GCed"
     (let [a (atom false)]
       (r/sample 100 (r/behavior (reset! a true)))
@@ -396,11 +399,11 @@
           e2 (r/events)
           j  (r/join e1 e2)]
       (push!! e1 1)
-      (is (= @j 1))
+      (is (= (deref! j) 1))
       (push!! e1 (r/completed 2))
-      (is (= @j 2))
+      (is (= (deref! j) 2))
       (push!! e2 3)
-      (is (= @j 3))))
+      (is (= (deref! j) 3))))
   (testing "blocking"
     (let [e1 (r/events)
           e2 (r/events)
@@ -408,18 +411,18 @@
           s  (r/reduce + j)]
       (push!! e1 1)
       (push!! e2 3)
-      (is (= @j 1))
-      (is (= @s 1))
+      (is (= (deref! j) 1))
+      (is (= (deref! s) 1))
       (push!! e1 (r/completed 2))
-      (is (= @j 3))
-      (is (= @s 6))))
+      (is (= (deref! j) 3))
+      (is (= (deref! s) 6))))
   (testing "complete"
     (let [e1 (r/events)
           e2 (r/events)
           j  (r/join e1 e2)]
       (push!! e1 (r/completed 1))
       (push!! e2 (r/completed 2))
-      (is (= @j 2))
+      (is (= (deref! j) 2))
       (is (r/complete? j)))))
 
 (deftest test-flatten
@@ -431,12 +434,12 @@
       (push!! es e1)
       (push!! e1 1)
       (is (realized? f))
-      (is (= @f 1))
+      (is (= (deref! f) 1))
       (push!! es e2)
       (push!! e2 2)
-      (is (= @f 2))
+      (is (= (deref! f) 2))
       (push!! e1 3)
-      (is (= @f 3))))
+      (is (= (deref! f) 3))))
   (testing "completion"
     (let [es (r/events)
           f  (r/flatten es)
